9af89bc05ce8231caaf829cf54cfb625eed65cb4
kerryf <kerry@tutanota.com>
2023-02-27 22:52:04 -0800

    Added csrf token generation

--- forest/auth_bp.py
+++ forest/auth_bp.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash
 
-from forest.guard import guest_required, login_required
+from forest.guard import generate_csrf_token, guest_required, login_required
 from forest.person_repo import find_person
 
 bp = Blueprint('auth', __name__)
@@ -11,23 +11,29 @@ bp = Blueprint('auth', __name__)
 @guest_required
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
+        # TODO: validate form data
 
         error = None
-        person = find_person(username)
+        person = find_person(email)
 
         if person is None:
-            error = 'Incorrect username.'
+            error = 'Incorrect username'
         elif not check_password_hash(person['password'], password):
-            error = 'Incorrect password.'
+            error = 'Incorrect password'
+
+        # TODO: check enabled, locked, and csrf token
 
         if error is None:
             session.clear()
             session['forest_person'] = person['id']
+            generate_csrf_token()
             return redirect(url_for('home.index'))
 
-        flash(error)
+        flash(error, 'login_error')
+    else:
+        generate_csrf_token()
 
     return render_template('login.html')
 
