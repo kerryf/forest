dd6ed9201f300c1c22d033800584f1c574b00115
kerryf <kerry@tutanota.com>
2023-02-27 22:49:43 -0800

    Added person_has_role function

--- forest/role_repo.py
+++ forest/role_repo.py
@@ -5,25 +5,23 @@ from forest.db import get_db
 
 def get_role(role_id: int) -> Optional[dict]:
     db = get_db()
-    role = db.execute('select * from role where id = ?', role_id).fetchone()
+    role = db.execute('select * from role where id = ?', (role_id,)).fetchone()
 
     return role
 
 
 def find_role(name: str) -> Optional[dict]:
     db = get_db()
-    role = db.execute('select * from role where name = ?', name).fetchone()
+    role = db.execute('select * from role where name = ?', (name,)).fetchone()
 
     return role
 
 
 def get_person_roles(person_id: int) -> list:
-    sql = '''select r.* from role r, person_role pr 
-    where r.id = pr.person_id and pr.person_id = ?
-    '''
+    sql = 'select r.* from role r, person_role pr where r.id = pr.role_id and pr.person_id = ?'
 
     db = get_db()
-    roles = db.execute(sql, person_id).fetchall()
+    roles = db.execute(sql, (person_id,)).fetchall()
 
     return roles
 
@@ -33,3 +31,16 @@ def add_person_role(person_id: int, role_id: int) -> None:
     db.execute('insert into person_role (person_id, role_id) values (?, ?)', (person_id, role_id))
 
     return None
+
+
+def person_has_role(person_id: int, roles: list[str]) -> bool:
+    args = [person_id]
+    args.extend(roles)
+
+    select = 'select 1 from role r, person_role pr where r.id = pr.role_id and pr.person_id = ?'
+    in_clause = ' and r.name in (%s)' % ','.join('?' for i in roles)
+
+    db = get_db()
+    has = db.execute(select + in_clause, args)
+
+    return has is not None
