ba1fd265f23f8a83bfe0d0c89e0982b104485245
kerryf <kerry@tutanota.com>
2023-03-02 08:06:48 -0800

    Added csrf and account enabled checks

--- forest/auth_bp.py
+++ forest/auth_bp.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash
 
-from forest.guard import generate_csrf_token, guest_required, login_required
+from forest.guard import check_csrf_token, generate_csrf_token, guest_required, login_required
 from forest.person_repo import find_person
+from forest.login_repo import record_action
 
 bp = Blueprint('auth', __name__)
 
@@ -11,6 +12,8 @@ bp = Blueprint('auth', __name__)
 @guest_required
 def login():
     if request.method == 'POST':
+        check_csrf_token(request.form['csrf_token'])
+
         email = request.form['email']
         password = request.form['password']
         # TODO: validate form data
@@ -19,20 +22,23 @@ def login():
         person = find_person(email)
 
         if person is None:
-            error = 'Incorrect username'
+            error = 'These credentials do not match our records'
         elif not check_password_hash(person['password'], password):
-            error = 'Incorrect password'
-
-        # TODO: check enabled, locked, and csrf token
+            error = 'These credentials do not match our records'
+        elif person['enabled'] == 0:
+            error = 'Your account is disabled. Please contact your system administrator.'
 
         if error is None:
+            # Start with a clean session
             session.clear()
             session['forest_person'] = person['id']
             generate_csrf_token()
+
             return redirect(url_for('home.index'))
 
         flash(error, 'login_error')
     else:
+        # Establish a 'pre-session' with a csrf token to be used for login requests
         generate_csrf_token()
 
     return render_template('login.html')
@@ -41,5 +47,15 @@ def login():
 @bp.post('/logout')
 @login_required
 def logout():
+
+    data = {
+        'person_id': session['forest_person'],
+        'action': 'logout',
+        'ip_address': request.remote_addr,
+        'user_agent': request.user_agent
+    }
+    record_action(data)
+
     session.clear()
+
     return redirect(url_for('forest.index'))
