c9138555247cc6aa431b7c17387a5355e3805e61
Kerry Friesen <Kerry.Friesen@ufv.ca>
2023-03-04 17:34:23 -0800

    Added handlers for mobile navigation and closing alerts

--- forest/static/js/forest.js
+++ forest/static/js/forest.js
@@ -1,6 +1,18 @@
 // Put your global JavaScript here
 
-u('#logout_link').on('click', function(e) {
-    e.preventDefault()
+/* Expand or collapse navigation on mobile */
+u('.navbar-burger').on('click', function () {
+    u('.navbar-burger').toggleClass('is-active')
+    u('.navbar-menu').toggleClass('is-active')
+})
+
+/* Handle the logout link */
+u('#logout_link').handle('click', function () {
     u('#logout_form').trigger('submit')
+})
+
+/* Enable the delete button on alerts */
+u('.notification .delete').on('click', function (e) {
+    let child = u(e.target)
+    child.parent().remove()
 })
